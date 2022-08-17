4a53628b1316d5682bbb4c23aeaf0bcaf355f1d0
Titus <111418016+TitusOniyal@users.noreply.github.com>
2022-08-17 17:29:25 +0530

    Add files via upload

---
+++ Polymorphism.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OOPS_in_C_Sharp
+{
+    /* Compile time ploymorphism /  Early binding / Method overloading / Static Binding  */
+    /* Same method but different signatures in same class */
+    public class Arithmetic
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Add(int a, int b, int c)
+        {
+            return a + b;
+        }
+    }
+
+
+    /* Runtime polymorphism / Late binding / Method overriding / Dynamic Binding */
+    /* Same method with same signatures but in different class */
+    public class Calc
+    {
+        public virtual void display() {
+            Console.WriteLine("Throws the basic method");
+        }
+    }
+    public class DerivedClac : Calc {
+        public override void display() {
+            Console.WriteLine("Throws the derived class method");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OOPS_in_C_Sharp
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Arithmetic _polyCompile = new Arithmetic();
+            _polyCompile.Add(5, 10);
+            _polyCompile.Add(5, 10, 15);
+
+            Calc _polyRuntime = new DerivedClac();
+            _polyRuntime.display();
+            Console.ReadLine();
+
+        }
+    }
+       
+}
